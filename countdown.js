c362e06b384e771fc759df2742da43145ffc1f6c
Bluemarker1025 <dbalak0924@gmail.com>
2021-02-28 19:34:44 -0500

    time

--- countdown.js
+++ countdown.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+var countDownDate = new Date("March 2, 2021 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -24,6 +24,6 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "McDonald";
   }
 }, 1000);
